290fd6332a9bb398aab2136ef7d114ad668b3561
MathewShuvarikov <shuvarikoff20@gmail.com>
2022-10-16 16:54:18 +0300

    Кластреризация банковских транзакций в R

---
+++ Clustering/Bank_transactions_clusters.R
@@ -0,0 +1,46 @@
+# download necessary libraries
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(psych)
+library(purrr)
+
+# download data
+df <- read_csv('/Users/user/Desktop/df/bank_transactions_2.csv')
+# glimpse on data
+glimpse(df)
+
+df_train <- df[,-4] # delete unnecessary columns
+df_train <- scale(df_train) # standardize data
+
+set.seed(123)
+
+# function to compute total within-cluster sum of square 
+wss <- function(k) {
+  kmedians(df_train, k, nstart = 10 )$tot.withinss
+}
+ 
+# Compute and plot wss for k = 1 to k = 15
+k.values <- c(1:15)
+
+# extract wss for 2-15 clusters
+wss_values <- map_dbl(k.values, wss)
+
+plot(k.values, wss_values,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+
+km <- kmedians(x = df_train, centers = 5) # train model
+
+df$cluster <- km$cluster # add cluster to initial data
+df %>% group_by(cluster) %>% summarise(median(gender), median(age), median(acc_balance), median(amount))
+#1-cluster: 42 year old men with 3 743 220 rupies account balance, and median transaction for 2500 rupies. 
+#2: 30 year old men with 8 194 rupies account balance, and median transaction for 230 rupies. 
+#3: 44 year old men with 30 128 rupies account balance, and median transaction for 700 rupies. 
+#4: 33 year old women with 17 848 rupies account balance, and median transaction for 490 rupies. 
+#5: 36 year old men with 15 159 rupies account balance, and median transaction for 396 rupies.
+
+# slightly different desults in Python and R, but commonly clusters from both python and r are similar.
+
+
