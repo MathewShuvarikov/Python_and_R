a0faf77c97fa4e8309c68f1837cb275949e61f37
MathewShuvarikov <shuvarikoff20@gmail.com>
2023-11-13 04:18:20 +0300

    Update files

--- Algorithms/Squared_array.py
+++ Algorithms/Squared_array.py
@@ -1,4 +1,4 @@
-
+## return squares of numbers from sorted array, ascending way
 x = [-5, -4, 0, 1,2,5]
 
 def solutiion(array: list[int]) -> list[int]:
@@ -7,4 +7,12 @@ def solutiion(array: list[int]) -> list[int]:
     return array
 
 print(solutiion(x))
+
+## return unique squares of numbers from sorted array,ascending way
+def solutiion2(array: list[int]) -> list[int]:
+    array = [num**2 for num in array]
+    array.sort()
+    return list(set(array))
+
+print(solutiion2(x))
 
